06c592b30d0fcff77dc2d14c3c52de7eea6536cf
Luke Hannigan <lukeyhannigany@gmail.com>
2022-09-01 23:13:39 +0100

    opt out skip allowance

--- contracts/ROY.sol
+++ contracts/ROY.sol
@@ -22,6 +22,8 @@ contract ROY is ERC20, AccessControl, CheckIns {
     
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);} 
 
+    bytes32 public constant OPT_OUT_SKIP_ALLOWANCE = keccak256("OPT_OUT_SKIP_ALLOWANCE");
+
     mapping(address => mapping(address => uint)) private _volume;
 
     function getVolume(address from, address to) external view returns(uint) {return _volume[from][to];}
@@ -32,8 +34,16 @@ contract ROY is ERC20, AccessControl, CheckIns {
 
     function burnFrom(address account, uint amount) external onlyRole(DEFAULT_ADMIN_ROLE) {_burn(account, amount);}
 
+    function optOutSkipAllowance() external {
+        _grantRole(OPT_OUT_SKIP_ALLOWANCE, msg.sender);
+    }
+
+    function optInSkipAllowance() external {
+        _revokeRole(OPT_OUT_SKIP_ALLOWANCE, msg.sender);
+    }
+
     function transferFrom(address from, address to, uint amount) public override returns(bool) {
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) return super.transferFrom(from, to, amount);
+        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(OPT_OUT_SKIP_ALLOWANCE, from)) return super.transferFrom(from, to, amount);
         _transfer(from, to, amount);
         return true;
     }
