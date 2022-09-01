9d6e97cc6473699ca3fb376fbddd2eadd1de3c40
Luke Hannigan <lukeyhannigany@gmail.com>
2022-09-02 00:05:42 +0100

    cannot revoke or grant skip allowance

--- contracts/ROY.sol
+++ contracts/ROY.sol
@@ -19,19 +19,23 @@ contract ROY is ERC20, AccessControl, CheckIns {
         uint amount;
     }
     
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);} 
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(TOKEN_MAESTRO_ROLE, msg.sender);
+    } 
 
     bytes32 public constant OPT_OUT_SKIP_ALLOWANCE = keccak256("OPT_OUT_SKIP_ALLOWANCE");
+    bytes32 public constant TOKEN_MAESTRO_ROLE = keccak256("TOKEN_MAESTRO_ROLE");
 
     mapping(address => mapping(address => uint)) private _volume;
 
     function getVolume(address from, address to) external view returns(uint) {return _volume[from][to];}
 
-    function mint(address account, uint amount) external onlyRole(DEFAULT_ADMIN_ROLE) {_mint(account, amount);}
+    function mint(address account, uint amount) external onlyRole(TOKEN_MAESTRO_ROLE) {_mint(account, amount);}
 
     function burn(uint amount) external {_burn(msg.sender, amount);}
 
-    function burnFrom(address account, uint amount) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function burnFrom(address account, uint amount) external onlyRole(TOKEN_MAESTRO_ROLE) {
         if(!_allowanceSkippable(account)) _spendAllowance(account, msg.sender, amount);
         _burn(account, amount);
     }
@@ -44,6 +48,20 @@ contract ROY is ERC20, AccessControl, CheckIns {
         _revokeRole(OPT_OUT_SKIP_ALLOWANCE, msg.sender);
     }
 
+    function grantRole(bytes32 role, address account) public override {
+        require(!_cannotGrantOrRevoke(role), "Cannot grant opt out skip allowance");
+        super.grantRole(role, account);
+    }
+
+    function revokeRole(bytes32 role, address account) public override {
+        require(!_cannotGrantOrRevoke(role), "Cannot revoke opt out skip allowance");
+        super.revokeRole(role, account);
+    }
+
+    function _cannotGrantOrRevoke(bytes32 role) private pure returns(bool) {
+        return role == OPT_OUT_SKIP_ALLOWANCE;
+    }
+
     function transferFrom(address from, address to, uint amount) public override returns(bool) {
         if(!_allowanceSkippable(from)) return super.transferFrom(from, to, amount);
         _transfer(from, to, amount);
@@ -51,7 +69,7 @@ contract ROY is ERC20, AccessControl, CheckIns {
     }
 
     function _allowanceSkippable(address account) private view returns(bool) {
-        return hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(OPT_OUT_SKIP_ALLOWANCE, account);
+        return hasRole(TOKEN_MAESTRO_ROLE, msg.sender) && !hasRole(OPT_OUT_SKIP_ALLOWANCE, account);
     }
 
     function multiTransfer(TransferInput[] calldata transfers) external {
