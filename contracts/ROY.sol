37b1f90481c1691aba0aa4b29c411b6dc4bc0467
Luke Hannigan <lukeyhannigany@gmail.com>
2022-09-01 22:42:18 +0100

    multi transfer

--- contracts/ROY.sol
+++ contracts/ROY.sol
@@ -8,6 +8,17 @@ import "./Checkins.sol";
 //importing checkins and inheriting from it (even if code is never used doesnt hurt to include)
 
 contract ROY is ERC20, AccessControl, CheckIns {
+
+    struct TransferInput {
+        address to;
+        uint amount;
+    }
+
+    struct TransferFromInput {
+        address from;
+        address to;
+        uint amount;
+    }
     
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);} 
 
@@ -20,6 +31,28 @@ contract ROY is ERC20, AccessControl, CheckIns {
     function burn(uint amount) external {_burn(msg.sender, amount);}
 
     function burnFrom(address account, uint amount) external onlyRole(DEFAULT_ADMIN_ROLE) {_burn(account, amount);}
+
+    function transferFrom(address from, address to, uint amount) public override returns(bool) {
+        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) return super.transferFrom(from, to, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function multiTransfer(TransferInput[] calldata transfers) external {
+        uint len = transfers.length;
+        for(uint i; i < len; i ++) {
+            TransferInput calldata transfer_ = transfers[i];  
+            transfer(transfer_.to, transfer_.amount);
+        }
+    }
+
+    function multiTransferFrom(TransferFromInput[] calldata transferFroms) external {
+        uint len = transferFroms.length;
+        for(uint i; i < len; i ++) {
+            TransferFromInput calldata transferFrom_ = transferFroms[i];  
+            transferFrom(transferFrom_.from, transferFrom_.to, transferFrom_.amount);
+        }
+    }
 
     function _afterTokenTransfer(address from, address to, uint amount) internal override {
         _volume[from][to] += amount;
